1e632af4a11b2ea88fb8d82be02314058f5ed96b
osenbal <enbal.blogs.215@gmail.com>
2025-12-27 17:02:11 +0700

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
+import node from "@astrojs/node"; // ⬅️ INI YANG KURANG
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: "server",
+  adapter: node({
+    mode: "standalone", // penting
+  }),
 });
