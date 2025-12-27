e806e850a189aed0f212677032ac9867f2f4fa5e
osenbal <enbal.blogs.215@gmail.com>
2025-12-27 17:26:43 +0700

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,13 @@ import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
-import vercel from "@astrojs/vercel/server";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: "server",
-  adapter: vercel(),
+  output: "hybrid",
+  adapter: vercel({
+    edgeMiddleware: true,
+  }),
 });
